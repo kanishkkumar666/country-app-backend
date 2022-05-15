6bee47558e344835d69f50df503ad2996ba9b993
kanishksoftobiz <kanishk@softobiz.com>
2022-05-15 20:43:24 +0530

    initial commit

--- backend/controller/countryController.js
+++ backend/controller/countryController.js
@@ -4,7 +4,11 @@ import CountryModel from "../models/countryModel.js";
 // import ConnectDB from "../connection/connectDB.js";
 
 const getCountry = asyncHandler(async (request, response) => {
-  response.setHeader("Access-Control-Allow-Origin", "https://country-app-frontend.herokuapp.com/");
+  request.setHeader("Access-Control-Allow-Origin", "https://country-app-frontend.herokuapp.com/");
+  request.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  request.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  request.setHeader("Access-Control-Allow-Credentials", true);
+  response.setHeader("Access-Control-Allow-Origin", "*");
   response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   response.setHeader("Access-Control-Allow-Headers", "Content-Type");
   response.setHeader("Access-Control-Allow-Credentials", true);
